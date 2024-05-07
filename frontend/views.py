e4499fa8c766a0bb8b43ee8764897d75ad5a2fa8
Oleksandr <Anulvl001@gmail.com>
2024-05-07 20:20:57 +0300

    add frontend auth

--- frontend/views.py
+++ frontend/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request, *args, **kwargs):
     return render(request, 'frontend/index.html')
+
+
+def index1(request, *args, **kwargs):
+    return render(request, 'frontend/index.html')
