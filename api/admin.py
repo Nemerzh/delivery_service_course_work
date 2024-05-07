e054cdefc05de143da2a6293f2481e22a10f9afe
Dimitrito <dima.2005.28.145@gmail.com>
2024-05-07 19:11:25 +0300

    Admin panel changes

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from api.models import Category
+from api.models import *
 
-# Register your models here.
 admin.site.register(Category)
-
+admin.site.register(Feedback)
+admin.site.register(UserRole)
+admin.site.register(Dish)
+admin.site.register(Admin)
+admin.site.register(User)
+admin.site.register(Customer)
+admin.site.register(Courier)
